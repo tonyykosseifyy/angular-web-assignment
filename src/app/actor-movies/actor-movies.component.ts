3237f3df0708cbf597f99b38f8b2cbf1e55b63dc
Tony Kosseify <tonykosseify123@gmail.com>
2024-05-17 16:25:31 +0300

    finish actor movies

--- src/app/actor-movies/actor-movies.component.ts
+++ src/app/actor-movies/actor-movies.component.ts
@@ -33,6 +33,21 @@ export class ActorMoviesComponent implements OnInit {
     const startIndex = (this.currentPage - 1) * 9;
     return this.movies.slice(startIndex, startIndex + 9);
   }
+  getStarRatingArray(popularity: number, maxPopularity: number = 100): any[] {
+    let normalizedPopularity = Math.min(popularity / maxPopularity, 1);
+    let starRating = normalizedPopularity * 5;
+    starRating = Math.round(starRating * 2) / 2;
+
+    const fullStars = Math.floor(starRating);
+    const halfStar = starRating % 1 !== 0 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStar;
+
+    return [
+      ...Array(fullStars).fill('full'),
+      ...Array(halfStar).fill('half'),
+      ...Array(emptyStars).fill('empty')
+    ];
+  }
 
   nextPage() {
     if (this.currentPage < this.totalPages) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,3 @@ export const environment = {
   production: false,
   apiKey: '30da36adc21eec7c8eea63844a3ff3f7'
 };
-  
